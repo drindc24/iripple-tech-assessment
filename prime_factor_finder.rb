203f599c12b3fbcac805f101812158cf215b625f
Aldrin Dela Cruz <adelacruz0930@gmail.com>
2021-05-13 17:32:13 +0800

    Add method to find prime factors

--- prime_factor_finder.rb
+++ prime_factor_finder.rb
@@ -1,3 +1,13 @@
-class PrimeFactorFinder
+require 'prime'
 
+class PrimeFactorFinder
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def prime_factors
+    number.prime_division.map(&:first)
+  end
 end
--- spec/prime_factor_finder_spec.rb
+++ spec/prime_factor_finder_spec.rb
@@ -1,5 +1,25 @@
 require_relative '../prime_factor_finder'
 
 RSpec.describe PrimeFactorFinder do
+  subject { described_class.new(10) }
 
+  describe '#initialize' do
+    it 'has a number attribute' do
+      expect(subject.number).to eq 10
+    end
+  end
+
+  describe '#prime_factors' do
+    it 'returns an array of all PRIME factors within the range of the given integer' do
+      expect(subject.prime_factors).to eq([2,5])
+    end
+
+    context '#large_integers' do
+      subject { described_class.new(2346623254324) }
+
+      it 'returns an array of all PRIME factors within the range of the given integer' do
+        expect(subject.prime_factors).to eq([2, 157, 3736661233])
+      end
+    end
+  end
 end
